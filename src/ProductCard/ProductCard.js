3c1f9ddc3875adc9de15f3946e78debac80e73ff
Torikul <toriqulislam142@gmail.com>
2022-03-25 23:58:34 +0600

    added font awesome and ui design completed

--- src/ProductCard/ProductCard.js
+++ src/ProductCard/ProductCard.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './ProductCard.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+  import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
 
 const ProductCard = (props) => {
-    const {name,id,image,price} = props.products
+    const {name,image,price} = props.products
     return (
         <div className='card'>
             <div className="card-info">
-                <img src={image} alt="" />
+                <div className="img"><img src={image} alt="mouses" /></div>
                 <h3>{name}</h3>
                 <h3> Price : $ {price}</h3>
-                <button>ADD TO CART</button>
+                <button>ADD TO CART  <FontAwesomeIcon icon={faCartShopping} /></button>
             </div>
         </div>
     );

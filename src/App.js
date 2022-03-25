96e226dc633b1a23dd5542ee8bfc01a41e5d7a56
Torikul <toriqulislam142@gmail.com>
2022-03-25 22:03:03 +0600

    header and data fetch complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 
+import { useEffect, useState } from 'react';
 import './App.css';
+import Header from './Components/Header';
 
 function App() {
-    
+  const [products, setProducts] = useState([])
+  useEffect(() => {
+    fetch('data.json')
+    .then(res => res.json())
+    .then(data => setProducts(data))
+  },[])
+  
+
   return (
     <div>
-      <h1> TECH TIMU STORE </h1>
+      <Header />
+      <div className='main-container'>
+            <div className="product-container">
+              
+            </div>
+          
+            <div className="cart-container">
+            
+            </div>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+    return (
+       
+     <div>
+       <h1 className='title'> MOUSE CORNER </h1>
+    </div>
+      
+    );
+};
+
+export default Header;
